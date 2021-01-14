6e2e35c5997c9cad45c6a2ed0e42eaaed04daeb6
Saksham Gupta <saksham1319@gmail.com>
2021-01-14 13:07:28 +0530

    hashroute

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {  HashRouter, Switch, Route } from 'react-router-dom';
 
 import Navi  from "./Components/Navi.js";
 import Pricing from "./Components/Pricing.js";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter basename="/">
+
         <Navi />
         <Switch>
           <Route path='/' exact component={Pricing} />
           <Route path='/pricing' exact component={Pricing} />
           
         </Switch>
-      </Router>
+      </HashRouter>
 
     </div>
   );
