89052a944f40537d122ea0967e3715fcb9e79aa7
Saksham Gupta <saksham1319@gmail.com>
2021-01-14 18:43:30 +0530

    .

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -15,11 +15,11 @@ function Card(props) {
     return (
         <div className="card" style={{border: "solid .5px rgb(185, 181, 181)",borderRadius:"10px"}}>
             <div className="cardTop">
-                <h2 style={{color:props.color, fontSize:"40px"}}>${props.cost}</h2>
+                <h1 style={{color:props.color, fontSize:"50px"}}>${props.cost}</h1>
                 <p>Per user,per month</p>
             </div>
             <div className="cardLower" style={{background: props.color}}>
-                <h3 >{props.plan}</h3>
+                <h2 >{props.plan}</h2>
                 {features.map((feature,i) =>(
                     <p style={ props.selected[i]==="1" ? {textDecoration:"none"} : {textDecoration:"line-through"}}>{feature}</p>
                 ))}
